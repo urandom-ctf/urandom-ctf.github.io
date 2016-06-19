d27567f3b459cb23e1b286be0d11e7fb61d3d6a3
mayth <chimeaquas@hotmail.com>
2016-06-20 02:28:30 +0900

    add log msg

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,16 +13,21 @@ get_hash() {
 publish() {
     commit_hash=$(get_hash)
     summary=$(get_summary)
+
+    echo "checking public dir"
     if (cd $ROOT_DIR/public && git diff --quiet); then
         echo "there are no changes to publish"
         return
     fi
+
+    echo "publish"
     (cd $ROOT_DIR/public && \
-        git diff --quiet && \
         git add . && \
         git commit -m "publish ${commit_hash}: \"${summary}\"" && \
         git push
     )
+
+    echo "update source repo"
     # update submodule
     (cd $ROOT_DIR && \
         git add public && \
